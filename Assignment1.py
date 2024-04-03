d2a6846a7dded21e7de1b463562e95a0faf61c06
unknown <m23csa019@iitj.ac.in>
2024-04-03 23:13:16 +0530

    File added

---
+++ Assignment1.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Activation functions
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+def leaky_relu(x, alpha=0.01):
+    return np.where(x > 0, x, alpha * x)
+
+def tanh(x):
+    return np.tanh(x)
+
+# Generating data
+x = np.linspace(-10, 10, 100)
+y_sigmoid = sigmoid(x)
+y_relu = relu(x)
+y_leaky_relu = leaky_relu(x)
+y_tanh = tanh(x)
+
+# Plotting
+plt.figure(figsize=(10, 6))
+
+plt.subplot(2, 2, 1)
+plt.plot(x, y_sigmoid, label='Sigmoid')
+plt.title('Sigmoid Activation Function')
+plt.legend()
+
+plt.subplot(2, 2, 2)
+plt.plot(x, y_relu, label='ReLU')
+plt.title('ReLU Activation Function')
+plt.legend()
+
+plt.subplot(2, 2, 3)
+plt.plot(x, y_leaky_relu, label='Leaky ReLU')
+plt.title('Leaky ReLU Activation Function')
+plt.legend()
+
+plt.subplot(2, 2, 4)
+plt.plot(x, y_tanh, label='Tanh')
+plt.title('Tanh Activation Function')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
